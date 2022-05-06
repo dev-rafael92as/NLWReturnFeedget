f92747156adbeaa53a1381f28938c186173e67e0
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-05-06 16:32:00 -0300

    Implementação principio SOLID para envio de emails e servidor para feedback

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "54cb1c9a68a94d",
+      pass: "61a4a7015afa65"
+    }
+  });
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({subject, body}: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Rafael Barros <rafa_barros92@hotmail.com>',
+            subject,
+            html: body,
+        })
+    }
+}
